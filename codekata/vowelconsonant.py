049f6e36942630dc1c904045a9690bd876e8b1d0
kirubah18 <51686056+kirubah18@users.noreply.github.com>
2019-06-12 10:49:28 +0530

    Create vowelconsonant.py

---
+++ codekata/vowelconsonant.py
@@ -0,0 +1,8 @@
+ki=input()
+if(ki.isalpha()):
+  if(ki=="a" or ki=="e" or ki=="i" or ki=="o" or ki=="u" or ki=="A" or ki=="E" or ki=="I" or ki=="O" or ki=="U"):
+    print("Vowel")
+  else:
+    print("Consonant")
+else:
+  print("invalid")

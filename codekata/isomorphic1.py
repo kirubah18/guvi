32e15e102a1ca5447f4a1203a4458b5b5437aeb8
kirubah18 <51686056+kirubah18@users.noreply.github.com>
2019-06-20 12:21:51 +0530

    Create isomorphic1.py

---
+++ codekata/isomorphic1.py
@@ -0,0 +1,9 @@
+si11, s2 = raw_input().split()
+count = 0
+for i in range(len(si11)):
+  if si11.count(si11[i]) == s2.count(s2[i]):
+    count += 1
+if count == len(si11):
+  print("yes")
+else:
+  print("no")

c3eb3de2ed1789c4bfed09c8945247bd8238cb81
kirubah18 <51686056+kirubah18@users.noreply.github.com>
2019-06-20 12:17:08 +0530

    Create ROMAN.py

---
+++ codekata/ROMAN.py
@@ -0,0 +1,5 @@
+lm1a=input()
+lm2=["0","I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"]
+for ii in range(21):
+  if(lm1a==lm2[ii]):
+    print(ii)
